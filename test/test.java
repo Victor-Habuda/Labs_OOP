35aa39b97fa0eb0e59595b9742ccd2b4661dd6a7
Victor PC <vic.habuda@gmail.com>
2026-02-03 17:33:02 +0200

    study 1

---
+++ test/test.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("Hello\n");
+        int a;
+        a = 52;
+        System.out.println(a);
+        a = 42;
+        System.out.println(a);
+        byte b = 127;
+        short s = 32767;
+        int i = 2000000000;
+        long l = 900000000;
+
+        float f = 4.5f;
+        double d = 5.6252552f;
+
+        char ch = 'b';
+        String str = "beans";
+
+        boolean bool = true;
+
+        Scanner input = new Scanner(System.in);
+        input.nextLine();
+    }
+}
