e3df361caf3c1fbc048ed2dbdfea10e56de5dd5a
Victor PC <vic.habuda@gmail.com>
2026-03-28 13:43:29 +0200

    Fixed logic and tests

--- src/test/java/lab1/lab1Test.java
+++ src/test/java/lab1/lab1Test.java
@@ -7,22 +7,32 @@ class Lab1Test {
 
     @Test
     void testXorOperations() {
+        Matrix obj = new Matrix();
+        double[][] matrixA = {{12.0, 14.0}, {16.0, 18.0}};
+        double[][] matrixB = {{0.0, 0.0}, {0.0, 0.0}};
 
-        double a11 = 12.0, a12 = 14.0, a21 = 16.0, a22 = 18.0;
-        double b11 = 0.0, b12 = 0.0, b21 = 0.0, b22 = 0.0;
+        obj.setMatrixA(matrixA);
+        obj.setMatrixB(matrixB);
 
+        obj.xor();
 
-//        double[][] result = lab1.xor(a11, a12, a21, a22, b11, b12, b21, b22);
+        double[][] result = obj.getMatrixC();
 
-        assertArrayEquals(new double[]{a11, a12}, result[0], "Рядок 0 не збігається");
-        assertArrayEquals(new double[]{a21, a22}, result[1], "Рядок 1 не збігається");
+        assertArrayEquals(matrixA[0], result[0], "Рядок 0 не збігається");
+        assertArrayEquals(matrixA[1], result[1], "Рядок 1 не збігається");
     }
 
     @Test
     void testXorIdenticalValues() {
-
+        Matrix obj = new Matrix();
         double val = 123.456;
-        double[][] result = lab1.xor(val, 0, 0, 0, val, 0, 0, 0);
+        double[][] matrixA = {{val, 0.0}, {0.0, 0.0}};
+        double[][] matrixB = {{val, 0.0}, {0.0, 0.0}};
+
+        obj.setMatrixA(matrixA);
+        obj.setMatrixB(matrixB);
+        obj.xor();
+        double[][] result = obj.getMatrixC();
 
         assertEquals(0.0, result[0][0], "XOR однакових double має повернути 0.0");
     }
